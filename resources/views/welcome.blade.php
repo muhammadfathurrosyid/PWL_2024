98484d8b47486604a1aa311297d6cd889cc0fac1
Fathur <rostafrevolution@gmail.com>
2025-03-15 14:26:04 +0700

    modifikasi tampilan awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PWL 2025 TI 23A2</title>
+        <title>PWL 2025</title>
     </head>
     <body>
         <h2>Pemrograman Web Lanjut 2025 TI 23A2</h2>
